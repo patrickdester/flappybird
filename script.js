aebe94b9c96d554d2cb308472209174fdd7922dd
Sam Louwers <90309503+patrickdester@users.noreply.github.com>
2021-09-24 10:42:13 +0000

    een vallend balletje gemaakt

--- script.js
+++ script.js
@@ -2,16 +2,16 @@ function setup() {
   createCanvas(600, 400);
 }
  
-var [xpos, ypos, xspeed, yspeed] = [125, 225, 0, 0];
+var [xpos, ypos, xspeed, yspeed] = [125, 225, 0, 10];
  
 function draw() {
-  background(100);
+  background(0);
   
   fill(0, 255, 0);
   circle(xpos, ypos, 25, 50);
   
   if(xpos >= 0 && xpos + 50 <= 500) xpos += xspeed;
-  if(ypos >= 0 && ypos + 50 <= 500) ypos += yspeed;
+  if(ypos >= 0 && ypos + 50 <= 400) ypos += yspeed;
 }
  
 function keyPressed() {
