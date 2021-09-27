aaf285b31047ba52ae5c094933e164833249cb36
Sam Louwers <90309503+patrickdester@users.noreply.github.com>
2021-09-27 07:58:39 +0000

    pijpen gemaakt en laten bewegen

--- script.js
+++ script.js
@@ -1,15 +1,23 @@
+var [xpos, ypos, xspeed, yspeed] = [125, 225, 0, 4];
+var pipe, pipe2;
+
 function setup() {
   createCanvas(600, 400);
+  pipe  = new Pipe(400);
+  pipe2 = new Pipe(700);
 }
  
-var [xpos, ypos, xspeed, yspeed] = [125, 225, 0, 4];
- 
+
+
 function draw() {
   background(0);
   
   fill(0, 255, 0);
   circle(xpos, ypos, 25, 50);
   
+  pipe.draw();
+  pipe2.draw();
+
   if(xpos >= 0 && xpos + 50 <= 500) xpos += xspeed;
   if(ypos >= 0 && ypos + 50 <= 400) ypos += yspeed;
 }
@@ -19,3 +27,14 @@ function keyPressed() {
    ypos -=100;
  }
 } 
+
+class Pipe {
+  constructor(startx  ){
+    this.x = startx;
+  }
+
+  draw(){
+    rect(this.x, 0, 50, 80);
+    this.x=this.x-1;
+  } 
+}
