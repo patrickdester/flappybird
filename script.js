4ca933bb1cb14f7f862c3439fc515e1cf77f9024
Sam Louwers <90309503+patrickdester@users.noreply.github.com>
2021-10-13 08:05:45 +0000

    gamestate en proberen buizn te laten werken

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-var [xpos, ypos, xspeed, yspeed] = [200, 225, 0, 4];
+var [xpos, ypos, xspeed, yspeed, acc] = [200, 225, 0, 0, 0.1];
 var pipe, pipe2, pipe3;
 var gameState = 1;
-
+var x = 0;
 var pipes = [];
 function setup() {
   createCanvas(600, 400);
@@ -9,6 +9,32 @@ function setup() {
 
 
 function draw() {
+
+  text("gameState" + gameState, 25, 25);
+
+  if (gameState == 0) {
+    menu();
+  }
+
+  if (gameState == 1) {
+    game();
+  }
+
+  if (gameState == 2) {
+    gameOver();
+  }
+}
+
+
+function menu() {
+  background("#ababab");
+  text("MENU", 25, 45);
+  text("1. menu", 25, 65);
+  text("2. start game", 25, 85);
+  text("3. game over", 25, 105);
+}
+
+function game() {
   background(0);
 
   fill("green");
@@ -19,19 +45,44 @@ function draw() {
     console.log("NIEUWE PIPE!")
   }
 
-
   pipes.forEach((p) => {
     p.draw()
     p.checkCollision();
   });
 
-  if (xpos >= 0 && xpos + 50 <= 500) xpos += xspeed;
-  if (ypos >= 0 && ypos + 50 <= 400) ypos += yspeed;
+  if (xpos >= 0 && xpos + 50 <= 500) {
+    xpos += xspeed
+  }
+  if (ypos >= 0 && ypos + 50 <= 400) {
+    if(yspeed < 5){
+      yspeed += acc;
+    }
+
+    ypos += yspeed
+  };
+}
+
+function gameOver() {
+  background("black");
+  text("GAME OVER", 25, 45);
+  x = 0;
 }
 
 function keyPressed() {
   if (keyCode == 32) {
     ypos -= 80;
+  }
+
+  if (keyCode == 49) {
+    gameState = 0;
+  }
+
+  if (keyCode == 50) {
+    gameState = 1;
+  }
+
+  if (keyCode == 51) {
+    gameState = 2;
   }
 }
 
@@ -54,9 +105,11 @@ class Pipe {
   }
 
   checkCollision() {
-    if (xpos - 25 > this.x && xpos + 25 > this.x + this.w) {
-       this.c = "red";
-      if (ypos > this.h && ypos + 50 < this.h + 150) {
+    if (xpos + 25 > this.x && xpos < this.x + this.w) {
+      this.c = "orange";
+      //gameState = 2;
+      ///console.log(ypos < this.h, ypos + 50 > this.h + 150)
+      if (ypos < this.h && ypos + 50 > this.h + 150) {
         this.c = "red";
       }
     }
